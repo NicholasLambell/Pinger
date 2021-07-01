4631581fb91cbcdfefda060e648390beba30e2e0
Nicholas Lambell <NicholasLambell@users.noreply.github.com>
2021-07-02 04:53:02 +1000

    Standardise vertical divider control

---
+++ Pinger/Control/VerticalDivider.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace Pinger.Control {
+    public partial class VerticalDivider : UserControl {
+        public VerticalDivider() {
+            InitializeComponent();
+        }
+    }
+}
